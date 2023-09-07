02731290f5f6b4132f7ec25c23e676a8db96c21d
RinMio2 <asd85111700@gmail.com>
2023-09-07 13:41:33 +0800

    小存檔

--- app/myproject/click.py
+++ app/myproject/click.py
@@ -3,9 +3,11 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
 class ClickEvent(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    # 添加其他字段，如时间戳、按钮ID等
-    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
-    button_id = db.Column(db.Integer)
-    # 添加其他字段，根据需要
+    click_id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    item_id = db.Column(db.Integer)
+
+    def __init__(self, id, item_id):
+        """初始化"""
+        self.email = id
+        self.username = item_id
